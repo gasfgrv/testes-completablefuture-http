0e1e38f13ce21032c01bcc92688972214979e572
Gustavo <gustavo_almeida11@hotmail.com>
2024-02-23 22:51:28 -0300

    arquivos iniciais

---
+++ src/main/java/com/github/gasfgrv/testescompletablefuturehttp/TestesCompletablefutureHttpApplication.java
@@ -0,0 +1,13 @@
+package com.github.gasfgrv.testescompletablefuturehttp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestesCompletablefutureHttpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestesCompletablefutureHttpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/gasfgrv/testescompletablefuturehttp/TestesCompletablefutureHttpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gasfgrv.testescompletablefuturehttp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestesCompletablefutureHttpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
